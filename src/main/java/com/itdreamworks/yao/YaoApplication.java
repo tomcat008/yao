a12d2cfbaaa1c6a9eeb75af536fd781bae40c9c0
Administrator <80201288@qq.com>
2018-04-08 15:02:39 +0800

    项目初始化

---
+++ src/main/java/com/itdreamworks/yao/YaoApplication.java
@@ -0,0 +1,12 @@
+package com.itdreamworks.yao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaoApplication.class, args);
+	}
+}
